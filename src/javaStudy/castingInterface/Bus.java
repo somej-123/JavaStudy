b71d70f47aded483f8e0b2cb3030c89e222f5d87
somej-123 <somej@naver.com>
2023-10-29 21:43:08 +0900

    커밋테스트

--- src/javaStudy/castingInterface/Bus.java
+++ src/javaStudy/castingInterface/Bus.java
@@ -3,7 +3,7 @@ package javaStudy.castingInterface;
 public class Bus implements Vehicle{
     @Override
     public void run(){
-        System.out.println("버스가 달립니다.");
+        System.out.println("버스가 달립니다.")
     }
 
     public void checkFare(){
